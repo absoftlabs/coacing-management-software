b5704ed76e8f9cea919d8d892c9066ebe23a4032
absoftlabs <abdullahtech2@outlook.com>
2025-09-29 15:21:31 +0600

    components file are created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html data-theme="forest" lang="en" data-arp="true">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
